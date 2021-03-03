c56553bd980782093419b78899d75f0586a88d22
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2021-03-03 13:35:37 +0000

    fix parcel build

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+import { initAll } from "lbh-frontend"
+
+//////////////////////////////////
+// JAVASCRIPT
+// Initialise your components here
+// Help: https://design-system.hackney.gov.uk/developing/installing-from-npm#using-javascript
+//////////////////////////////////
+
+initAll()
